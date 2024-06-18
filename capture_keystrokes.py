74a171b0f69a7234371994acdf27da5682e87f32
Ishika <ishika02908@gmail.com>
2024-06-18 21:13:37 +0530

    capture keystrokes

---
+++ capture_keystrokes.py
@@ -0,0 +1,16 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        print(f"Key {key.char} pressed")
+    except AttributeError:
+        print(f"Special key {key} pressed")
+
+def on_release(key):
+    print(f"Key {key} released")
+    if (key == keyboard.Key.esc):
+        # Stop Listener
+        return False
+
+with keyboard.Listener(on_press, on_release) as listener:
+    listener.join()
